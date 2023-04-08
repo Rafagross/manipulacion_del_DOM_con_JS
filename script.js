9adb8d89bb3f9e54d7f0fd5d4716da4a7f532869
Rafagross <rafagross15@gmail.com>
2023-04-08 14:16:22 -0500

    terminado

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-(() => {
-  const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-  const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-    const taskContent = document.createElement("div");
+const btn = document.querySelector("[data-form-btn]");
 
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    const content = `
-    ${checkComplete()}
-    <span class="task">${value}</span>  
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //task.innerHTML = content;
-    task.appendChild(taskContent);
-    list.appendChild(task);
-  };
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  const taskContent = document.createElement("div");
 
-  btn.addEventListener("click", createTask);
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
 
-  const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    return i;
-  };
+  //task.innerHTML = content;
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+  list.appendChild(task);
+};
 
-  const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-  };
-})();
+btn.addEventListener("click", createTask);
